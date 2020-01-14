c8ab12ee4f2399f67cd1f18233185509d380428a
Dhone <dhoneakshay39@gmail.com>
2020-01-14 20:02:30 +0530

    done with sign up and sign in page

--- app/src/main/java/com/example/stackinest/HomeActivity.java
+++ app/src/main/java/com/example/stackinest/HomeActivity.java
@@ -1,13 +1,54 @@
 package com.example.stackinest;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button logout;
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        getSupportActionBar().setTitle("HomePage");
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        logout=(Button)findViewById(R.id.logout);
+        mAuth=FirebaseAuth.getInstance();
+
+        mAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+
+                if(firebaseAuth.getCurrentUser()==null){
+
+                    startActivity(new Intent(HomeActivity.this,SignIn.class));
+                }
+            }
+        };
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+
+            }
+        });
+
     }
 }
