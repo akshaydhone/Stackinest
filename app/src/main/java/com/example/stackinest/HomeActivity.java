9977fafc0b0a1221c739ee8a09c6db7174b8220d
Dhone <dhoneakshay39@gmail.com>
2020-02-06 17:39:29 +0530

    added home page screen

--- app/src/main/java/com/example/stackinest/HomeActivity.java
+++ app/src/main/java/com/example/stackinest/HomeActivity.java
@@ -4,15 +4,19 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
     Button logout;
     FirebaseAuth mAuth;
+    TextView username;
     FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
@@ -21,13 +25,18 @@ public class HomeActivity extends AppCompatActivity {
         mAuth.addAuthStateListener(mAuthListener);
     }
 
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         getSupportActionBar().setTitle("HomePage");
+        username=(TextView)findViewById(R.id.username);
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        logout=(Button)findViewById(R.id.logout);
+        //logout=(Button)findViewById(R.id.logout);
         mAuth=FirebaseAuth.getInstance();
 
         mAuthListener=new FirebaseAuth.AuthStateListener() {
@@ -42,13 +51,29 @@ public class HomeActivity extends AppCompatActivity {
             }
         };
 
-        logout.setOnClickListener(new View.OnClickListener() {
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        Log.d("LOGGED", "FirebaseUser: " + user);
+
+        if (user != null) {
+            username.setText("Welcome, " + user.getDisplayName());
+
+
+
+           // MainActivity.LoggedIn_User_Email =user.getDisplayName();
+
+
+
+
+        }
+
+       /* logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mAuth.signOut();
 
             }
-        });
+        });*/
 
     }
 }
--- app/src/main/java/com/example/stackinest/MainActivity.java
+++ app/src/main/java/com/example/stackinest/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     Button b1,b2;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
